5abe9661a4be985ec811793a66b70427cab1c4c4
Anqi Wang <aw222@ic.ac.uk>
2022-10-13 12:27:02 +0100

    python2

---
+++ week2/code/cfexercises2.py
@@ -0,0 +1,13 @@
+###########################
+def hello_1(x):
+    for j in range(x):
+        if j % 3 == 0:
+            print('hello')
+
+hello_1(12)
+
+##########################
+def hello_2(x):
+    for j in range(x):
+        if j % 5 == 3:
+        
